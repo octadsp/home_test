c1ab0e9b0349557732c9944c2b5fb9b3e0ecc2b6
tole <octaadsp.camp@gmail.com>
2023-06-16 12:16:09 +0700

    CORS

--- server-home-test/main.go
+++ server-home-test/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/octadsp/server-home-test/database"
 	connectiondb "github.com/octadsp/server-home-test/pkg/connectionDB"
@@ -17,13 +18,21 @@ func main() {
 	database.RunMigration()
 
 	router := gin.New()
-	routes.RouteInit(router.Group("/api/v1"))
 
-	// var PORT = os.Getenv("PORT")
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"X-Requested-With", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: false,
+	}))
+
+	routes.RouteInit(router.Group("/api/v1"))
 
 	fmt.Println("Server running localhost :" + "5000")
 	// g.Logger.Fatal(g.Start(":" + PORT))
 	router.Use(gin.Logger())
+
+	router.Static("/uploads", "./uploads")
 
 	err := router.Run(":" + "5000")
 	if err != nil {
